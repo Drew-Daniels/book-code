9115fac1c459cd7b1314a6383f972d244cebae7b
Drew Daniels <daniels_drew@protonmail.com>
2024-09-10 06:48:04 -0500

    add pending tests

--- 02-running-specs/09/spec/coffee_spec.rb
+++ 02-running-specs/09/spec/coffee_spec.rb
@@ -8,7 +8,7 @@ class Coffee
   end
 
   def price
-    1.00 + ingredients.size * 0.25
+    1.00 + (ingredients.size * 0.25)
   end
 end
 
@@ -31,7 +31,15 @@ RSpec.describe 'A cup of coffee' do
       expect(coffee.price).to eq(1.25)
     end
 
-    it 'is light in color'
-    it 'is cooler than 200 degrees Fahrenheit'
+    it 'is light in color' do
+      pending 'Color not implemented yet'
+      expect(coffee.color).to be(:light)
+    end
+
+    it 'is cooler than 200 degrees Fahrenheit' do
+      pending 'Temperature not implemented yet'
+
+      expect(coffee.temperature).to be < 200.0
+    end
   end
 end
